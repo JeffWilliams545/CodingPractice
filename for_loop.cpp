dacb7916aaff56f5f5dc07728b16ad13ae809e7c
Jeffrey Williams <JeffWilliams545@gmail.com>
2018-04-09 19:07:21 -0700

    Add files via upload

---
+++ for_loop.cpp
@@ -0,0 +1,16 @@
+//Author Jeffrey Williams
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+    int input_A, input_B;
+
+    cout << "Please enter two natural numbers, the first must be less than or equal to the second." << endl;
+    cin >> input_A;
+    cin >> input_B;
+
+    return 1;
+}
